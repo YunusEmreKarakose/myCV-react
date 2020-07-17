a40924b4b1e3ee7c2490047aff9061bc96cd4dca
Yunus Emre Karaköse <yemre05@hotmail.com.tr>
2020-07-17 19:04:20 +0300

    1

--- src/components/skills.js
+++ src/components/skills.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Skills() {
   return (
     <div>
-        <div class="featured pad" id="skills">
+        <div class="pad" id="skills">
             <div class="container">
                 
                 <div class="default-heading">                    
@@ -11,56 +11,54 @@ function Skills() {
                 </div>
                     <h4>Programming Languages</h4>
                     <ul >
-                        <li class="skill"><img src="img/skills/c.png"  ></img></li>
-                        <li class="skill"><img src="img/skills/c++.png"></img></li>
+                        <li class="skill"><img class="skillimg"  src="img/skills/c.png"  ></img></li>
+                        <li class="skill"><img class="skillimg"  src="img/skills/c++.png"></img></li>
                         <li class="skill">
                             <a href="https://github.com/YunusEmreKarakose/PythonServer">
-                                <img src="img/skills/python.png"></img>
+                                <img class="skillimg"  src="img/skills/python.png"></img>
                             </a>
                         </li>
-                        <li class="skill"><img src="img/skills/js.png"></img></li>
+                        <li class="skill"><img class="skillimg"  src="img/skills/js.png"></img></li>
                     </ul>
                     <h4> Runtime Environment/Frameworks/API </h4>
                     <ul >
                         <li class="skill">
                             <a href="https://github.com/YunusEmreKarakose/KtuMebsis">
-                            <img src="img/skills/nodejs.png"  ></img>
+                            <img class="skillimg"src="img/skills/nodejs.png"  ></img>
                             </a>
                         </li>
                         <li class="skill">
                             <a href="https://github.com/YunusEmreKarakose/KtuMebsis">
-                                <img src="img/skills/expressjs.png"></img>
+                                <img class="skillimg" src="img/skills/expressjs.png"></img>
                             </a>
                         </li>
                         <li class="skill">
                             <a href="#">
-                                <img src="img/skills/react.png"></img>
+                                <img class="skillimg" src="img/skills/react.png"></img>
                             </a>
                         </li>
                         <li class="skill">
                             <a href="https://github.com/YunusEmreKarakose/ParalelComputing">
-                                <img src="img/skills/openmp.png" ></img>
+                                <img  class="skillimg"src="img/skills/openmp.png" ></img>
                             </a>
                         </li>
                     </ul>
                     <h4>Databases</h4>
                     <ul >
-                        <li class="skill"><img src="img/skills/mysql.png"  ></img></li>
-                        <li class="skill"><img src="img/skills/mongo.png"></img></li>
+                        <li class="skill"><img  class="skillimg" src="img/skills/mysql.png"  ></img></li>
+                        <li class="skill"><img  class="skillimg" src="img/skills/mongo.png"></img></li>
                     </ul>
                     <h4>Other</h4>
                     <ul >
-                        <li class="skill"><img src="img/skills/lora.png"></img></li>
+                        <li class="skill"><img class="skillimg"  src="img/skills/lora.png"></img></li>
                     </ul>
                 
             </div>
         </div>
-        <div id="skilldetail"class="cta parallax-two pad">
-            <div class="container">
-                <div class="cta-element ">
-                    <div class="row">
-                        <h1>SkillDetails</h1>                        
-                    </div>
+        <div id="skilldetail"class="promo parallax-two pad">
+            <div class="container">                
+                <div class="default-heading-shadow">                    
+                    <h2>skilldetail</h2>                    
                 </div>
                 <ul class="skilldetail">
                     <li class="skilldetail"> C/C++ : Algorithm implementation and aplications. Some OOP based aplications. </li>
