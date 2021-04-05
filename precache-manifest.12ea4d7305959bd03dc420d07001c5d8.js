9fc0d9aecab8d81e8fbbc3ff899282a6eb120d94
Yunus Emre Karaköse <yemre05@hotmail.com.tr>
2021-04-05 15:29:09 +0300

    Updates

---
+++ precache-manifest.12ea4d7305959bd03dc420d07001c5d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455bcc1a7affedb0c35966d895559c11",
+    "url": "/myCV-react/index.html"
+  },
+  {
+    "revision": "36664a40364ba36c3412",
+    "url": "/myCV-react/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e522ffaa0827a6e8f6f8",
+    "url": "/myCV-react/static/js/2.94514c78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myCV-react/static/js/2.94514c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36664a40364ba36c3412",
+    "url": "/myCV-react/static/js/main.67b20577.chunk.js"
+  },
+  {
+    "revision": "e43b2ef116f95843dc1e",
+    "url": "/myCV-react/static/js/runtime-main.72db7918.js"
+  }
+]);
